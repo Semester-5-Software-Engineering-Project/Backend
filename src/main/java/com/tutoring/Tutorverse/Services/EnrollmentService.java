a43e260a76dfa0b54f3f45bd9520f10be171cefa
Tiran Vitharana <vitharanats.22@uom.lk>
2025-10-02 23:59:00 +0530

    Fixed some confliclts with rating

--- src/main/java/com/tutoring/Tutorverse/Services/EnrollmentService.java
+++ src/main/java/com/tutoring/Tutorverse/Services/EnrollmentService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import com.tutoring.Tutorverse.Dto.EnrollGetDto;
 import com.tutoring.Tutorverse.Dto.ModuelsDto;
 import com.tutoring.Tutorverse.Model.User;
 import com.tutoring.Tutorverse.Repository.userRepository;
@@ -97,8 +96,8 @@ public class EnrollmentService {
     }
 
     public UUID getEnrollmentId(UUID userId, UUID fromString) {
-        EnrollmentEntity enrollment = enrollRepository.findByStudentStudentIdAndModuleModuleId(userId, fromString);
-        return enrollment != null ? enrollment.getEnrolmentId() : null;
+        Optional<EnrollmentEntity> enrollment = enrollRepository.findByStudentStudentIdAndModuleModuleId(userId, fromString);
+        return enrollment.map(EnrollmentEntity::getEnrolmentId).orElse(null);
     }
 
 
